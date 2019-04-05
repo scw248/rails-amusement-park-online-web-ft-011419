162768b27b72fbcabda663664c10305d1a5c554b
github username <github email address>
2019-04-05 15:27:40 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if nausea > happiness
+      'sad'
+    else
+      'happy'
+    end
+  end
+
+  def user
+    self
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  resources :users
+  resources :attractions
+
+  get '/signin', to: 'sessions#new'
+
+  post '/sessions', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
+  post '/users/ride', to: 'users#ride'
+
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
